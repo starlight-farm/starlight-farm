cffe69a0580b368a340d159b31e9600221c5fe20
starlight-farm <sl-farm@naver.com>
2026-06-18 16:06:28 +0900

    robots 추가

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://별빛목장.kr/sitemap.xml",
+  };
+}
